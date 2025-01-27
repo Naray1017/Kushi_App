cd111b026046ff7fb771674a873af23b7c31e69c
prathapramM <prathapram039@gmail.com>
2025-01-28 00:21:32 +0530

    code of statistics

--- src/main/java/com/kushi/in/kushi/KushiApplication.java
+++ src/main/java/com/kushi/in/kushi/KushiApplication.java
@@ -2,11 +2,14 @@ package com.kushi.in.kushi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaRepositories("com.kushi.in.kushi.repository") 
 public class KushiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(KushiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KushiApplication.class, args);
+    }
 }
